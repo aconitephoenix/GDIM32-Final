0752f9dfba0de5eb8541be2c0f8f7b768f90798c
aconitephoenix <jennythanhvi@gmail.com>
2026-02-18 00:18:58 -0800

    added ui script skeleton -jess

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _pagesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
