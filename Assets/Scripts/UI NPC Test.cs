add6eddb8e962e0a102e7c47c7af23a144560856
aconitephoenix <jennythanhvi@gmail.com>
2026-02-25 01:03:52 -0800

    basic dialogue branch testing -jess

---
+++ Assets/Scripts/UI NPC Test.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UINPCTest : MonoBehaviour
+{
+    [SerializeField] private float _interactionDistance = 2.0f;
+    [SerializeField] private UIController _uiController;
+    [SerializeField] private string _name;
+    [SerializeField] private DialogueNode _startingNode;
+
+    private DialogueNode _currentNode;
+    private int _currentLine = 0;
+    private bool _waitingForPlayerResponse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentNode = _startingNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.Instance.Player == null) return;
+
+        if (Vector3.Distance(transform.position, GameController.Instance.Player.transform.position) <= _interactionDistance)
+        {
+            if (!_waitingForPlayerResponse && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E)) && gameObject.CompareTag("NPC"))
+            {
+                AdvanceDialogue();
+            }
+        } else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void AdvanceDialogue()
+    {
+        if (_currentLine < _currentNode._lines.Length)
+        {
+            // keep playing NPC lines if there are still any left
+            _uiController.ShowDialogue(_currentNode._lines[_currentLine], _name);
+            _currentLine++;
+        }
+        else if (_currentNode._playerReplyOptions != null && _currentNode._playerReplyOptions.Length > 0)
+        {
+            // show player dialogue options, if any
+            _waitingForPlayerResponse = true;
+            _uiController.ShowPlayerOptions(_currentNode._playerReplyOptions);
+        }
+        else
+        {
+            // end dialogue if none left
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        _waitingForPlayerResponse = false;
+        _currentNode = _startingNode;
+        _currentLine = 0;
+        _uiController.HideDialogue();
+    }
+
+    public void SelectedOption(int option)
+    {
+        _currentLine = 0;
+        _waitingForPlayerResponse = false;
+
+        _currentNode = _currentNode._npcReplies[option];
+        AdvanceDialogue();
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,6 +11,11 @@ public class UIController : MonoBehaviour
     [SerializeField] private GameObject _dialogueBox;
     [SerializeField] private GameObject _playerOptions;
     [SerializeField] private GameObject _sprintBar;
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _option1;
+    [SerializeField] private TMP_Text _option2;
+
+    private int _pageNumber = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +35,11 @@ public class UIController : MonoBehaviour
         switch (tag)
         {
             case "NPC":
-                _hoverText.text = "Click to talk";
+                _hoverText.text = "Click or Press E to talk";
                 _hoverText.gameObject.SetActive(true);
                 break;
             case "Interactable":
-                _hoverText.text = "Click to interact";
+                _hoverText.text = "Click or Press E to interact";
                 _hoverText.gameObject.SetActive(true);
                 break;
             default:
@@ -43,27 +48,51 @@ public class UIController : MonoBehaviour
         }
     }
 
-    public void ShowDialogue(string dialogue)
+    public void ShowDialogue(string dialogue, string name)
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         _dialogueBox.SetActive(true);
         _playerOptions.SetActive(false);
         _sprintBar.SetActive(false);
         _hoverText.gameObject.SetActive(false);
 
         _dialogueText.text = dialogue;
+        _nameText.text = name;
     }
 
     public void HideDialogue()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         _dialogueBox.SetActive(false);
         _playerOptions.SetActive(false);
         _sprintBar.SetActive(true);
     }
 
-    public void ShowPlayerOptions()
+    public void ShowPlayerOptions(string[] options)
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         _playerOptions.SetActive(true);
         _sprintBar.SetActive(false);
         _hoverText.gameObject.SetActive(false);
+
+        _option1.text = options[0];
+
+        if (options.Length >= 2)
+        {
+            _option2.transform.parent.gameObject.SetActive(true);
+            _option2.text = options[1];
+        }
+        else
+        {
+            _option2.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdatePageNumber()
+    {
+
     }
 }
