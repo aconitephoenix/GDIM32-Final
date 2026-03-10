c65801ffa04dbc17854dff5e94edb915c5008a4a
aconitephoenix <jennythanhvi@gmail.com>
2026-03-09 23:45:23 -0700

    Update Freddy.cs

--- Assets/Scripts/Freddy.cs
+++ Assets/Scripts/Freddy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Freddy : MonoBehaviour
+public class Freddy : NPC
 {
     // Start is called before the first frame update
     void Start()
